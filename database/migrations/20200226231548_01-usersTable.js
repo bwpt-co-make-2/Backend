4c8a9138246e358b3f2d2b850af0f0676325ef40
Th5tch3r <amber.phan27@gmail.com>
2020-02-28 19:51:43 -0500

    create tables for upvote and issues

--- database/migrations/20200226231548_01-usersTable.js
+++ database/migrations/20200226231548_01-usersTable.js
@@ -6,11 +6,52 @@ exports.up = function(knex) {
             tbl.string('username', 128)
                 .notNullable()
                 .unique();
-            tbl.string('password', 265)
+            tbl.string('password', 256)
                 .notNullable();
+            tbl.integer('zipcode', 5)
+                .notNullable();
+            tbl.boolean('government official', false);
+        })
+
+        .createTable('issues', tbl => {
+            tbl.increments();
+            tbl.string('title', 256)
+                .notNullable();
+            tbl.string('description', 500)
+                .notNullable();
+            tbl.string('picture', 500);
+            tbl.integer('zipcode', 5);
+            tbl.integer('users_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
+
+        .createTable('upvote', tbl => {
+            tbl.increments();
+            tbl.integer('users_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.integer('issues_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('issues')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
         })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('users')
+    return knex.schema
+        .dropTableIfExists('upvote')
+        .dropTableIfExists('issues')
+        .dropTableIfExists('users')
 };
